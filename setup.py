8463d0ac4500b4b85ca82a826efd5fb4806af1c9
Michaël BAUDIN <michael.baudin@gmail.com>
2020-04-04 20:53:10 +0200

    Adding MANIFEST

--- setup.py
+++ setup.py
@@ -14,28 +14,24 @@ Usage
 """
 from setuptools import (setup, find_packages)
 
-install_requires = ['numpy>=1.13',
-                    'matplotlib>=2.1',
-                    'openturns>=1.14'
-                    ]
-extras_require = {'doc': ['sphinx>=1.4', 'nbsphinx', 'jupyter', 'jupyter_client']}
-
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(
     name='othdrplot',
     keywords=("graphics"),
-    version='0.2.3',
-    packages=find_packages(exclude=['doc']),
-    install_requires=install_requires,
-    extras_require=extras_require,
-    description="othdrplot: HDR plot",
+    version='0.0.1',
+    packages=find_packages(),
+    requires=['numpy',
+              'matplotlib',
+              'openturns'
+              ],
+    description="High Density Region plot",
     long_description=long_description,
     long_description_content_type="text/markdown",
     classifiers=['Development Status :: 5 - Production/Stable',
                  'Environment :: Console',
-                 'License :: OSI Approved',
+                 'License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)',
                  'Intended Audience :: Science/Research',
                  'Intended Audience :: Developers',
                  'Natural Language :: English',
@@ -47,7 +43,8 @@ setup(
                  'Topic :: Scientific/Engineering',
                  ],
     include_package_data=True,
-    zip_safe=False,
     license="LGPL",
     url="https://github.com/mbaudin47/othdrplot",
+    author="Michaël Baudin and Pamphile Roy",
+    author_email="michael.baudin@gmail.com", 
 )
