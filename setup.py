38c1ceb269e07ac4daa841e0709b2b4b45b208b9
Michael BAUDIN <michael.baudin@gmail.com>
2021-02-12 18:24:59 +0100

    Fixed setup

--- setup.py
+++ setup.py
@@ -20,9 +20,9 @@ with open("README.md", "r") as fh:
 setup(
     name="othdrplot",
     keywords=("graphics"),
-    version="1.1",
+    version="2.0",
     packages=find_packages(),
-    install_requires=["numpy", "matplotlib", "openturns"],
+    install_requires=["numpy", "matplotlib", "openturns>=1.16"],
     description="High Density Region plot",
     long_description=long_description,
     long_description_content_type="text/markdown",
